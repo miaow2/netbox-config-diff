d8086896435f7b3c40a1139abfde8c67cec4a5d0
Artem Kotik <miaow2@yandex.ru>
2023-09-29 20:27:55 +0400

    Version 1.2.2

--- netbox_config_diff/__init__.py
+++ netbox_config_diff/__init__.py
@@ -2,7 +2,7 @@ from extras.plugins import PluginConfig
 
 __author__ = "Artem Kotik"
 __email__ = "miaow2@yandex.ru"
-__version__ = "1.2.1"
+__version__ = "1.2.2"
 
 
 class ConfigDiffConfig(PluginConfig):
