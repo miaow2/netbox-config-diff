1a3a4a782ebf65f23a2088f96ee0980c0e92b726
Artem Kotik <miaow2@yandex.ru>
2024-01-28 15:13:22 +0400

    Move plugin into separate menu item

--- netbox_config_diff/navigation.py
+++ netbox_config_diff/navigation.py
@@ -1,4 +1,4 @@
-from extras.plugins import PluginMenuButton, PluginMenuItem
+from extras.plugins import PluginMenu, PluginMenuButton, PluginMenuItem
 from utilities.choices import ButtonColorChoices
 
 
@@ -12,35 +12,36 @@ def get_add_button(model: str) -> PluginMenuButton:
     )
 
 
-menu_items = (
-    PluginMenuItem(
-        link="plugins:netbox_config_diff:platformsetting_list",
-        link_text="Platform Settings",
-        buttons=[get_add_button("platformsetting")],
-        permissions=["netbox_config_diff.view_platformsetting"],
-    ),
-    PluginMenuItem(
-        link="plugins:netbox_config_diff:configcompliance_list",
-        link_text="Config Compliances",
-        buttons=[],
-        permissions=["netbox_config_diff.view_configcompliance"],
-    ),
-    PluginMenuItem(
-        link="plugins:netbox_config_diff:configurationrequest_list",
-        link_text="Configuration Requests",
-        buttons=[get_add_button("configurationrequest")],
-        permissions=["netbox_config_diff.view_configurationrequest"],
-    ),
+def get_menu_item(model: str, verbose_name: str, add_button: bool = True) -> PluginMenuItem:
+    return PluginMenuItem(
+        link=f"plugins:netbox_config_diff:{model}_list",
+        link_text=verbose_name,
+        buttons=[get_add_button(model)] if add_button else [],
+        permissions=[f"netbox_config_diff.view_{model}"],
+    )
+
+
+compliance_items = (
+    get_menu_item("platformsetting", "Platform Settings"),
+    get_menu_item("configcompliance", "Config Compliances", add_button=False),
+)
+
+config_items = (
+    get_menu_item("configurationrequest", "Configuration Requests"),
+    get_menu_item("substitute", "Substitutes"),
     PluginMenuItem(
         link="plugins:netbox_config_diff:configurationrequest_job_list",
         link_text="Jobs",
         buttons=[],
         permissions=["core.view_job"],
     ),
-    PluginMenuItem(
-        link="plugins:netbox_config_diff:substitute_list",
-        link_text="Substitutes",
-        buttons=[get_add_button("substitute")],
-        permissions=["netbox_config_diff.view_substitute"],
+)
+
+menu = PluginMenu(
+    label="Config Diff Plugin",
+    groups=(
+        ("Compliance", compliance_items),
+        ("Config Management", config_items),
     ),
+    icon_class="mdi mdi-vector-difference",
 )
