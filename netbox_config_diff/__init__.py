a440ae700dbca8cf8110d654c99c00ca625bbc88
Artem Kotik <miaow2@yandex.ru>
2025-08-31 14:58:51 +0200

    Bump version to 2.11.0

--- netbox_config_diff/__init__.py
+++ netbox_config_diff/__init__.py
@@ -2,7 +2,7 @@ from netbox.plugins import PluginConfig
 
 __author__ = "Artem Kotik"
 __email__ = "miaow2@yandex.ru"
-__version__ = "2.10.0"
+__version__ = "2.11.0"
 
 
 class ConfigDiffConfig(PluginConfig):
