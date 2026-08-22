d845a4e306084414844addb1de751de52428b1e4
Artem Kotik <miaow2@yandex.ru>
2026-08-22 16:58:17 +0200

    Update version to 2.15.2

--- netbox_config_diff/__init__.py
+++ netbox_config_diff/__init__.py
@@ -2,7 +2,7 @@ from netbox.plugins import PluginConfig
 
 __author__ = "Artem Kotik"
 __email__ = "miaow2@yandex.ru"
-__version__ = "2.15.1"
+__version__ = "2.15.2"
 
 
 class ConfigDiffConfig(PluginConfig):
